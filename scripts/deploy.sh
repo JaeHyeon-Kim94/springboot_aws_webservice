bdd4cfcae0303f67ff59f96a6e7f23912ef42663
kimjaehyeon <daxxx5428@naver.com>
2022-10-31 17:59:11 +0900

    start.sh edit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,10 +33,7 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location="classpath:/application.properties,\
-  classpath:/application-real.properties,\
-  /home/ec2-user/app/application-oauth.properties,\
-  /home/ec2-user/app/application-real-db.properties" \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
   #2>&1 :  표준 에러 리다이렉션.
